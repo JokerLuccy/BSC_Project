d1607775b135b02580afd240bc07225548a2b00b
JokerLuccy <1031966036@qq.com>
2021-10-23 10:40:00 +0800

    feat:增加拦截器请求

---
+++ src/utils/http.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import Nprogress from "nprogress";
+import { Notify, Toast } from "vant";
+const request = axios.create({
+  baseURL: process.env.VU_APP_BASE_URL,
+  timeout: 1500,
+  timeoutErrorMessage: "网络错误，请刷新重试",
+});
+request.interceptors.request.use(
+  (config) => {
+    Nprogress.start();
+    return Promise.resolve(config);
+  },
+  (error) => Promise.reject(error)
+);
+request.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      if (response.data.code) {
+        Toast.fail(response.data.message);
+      } else {
+        return Promise.resolve(response.data.data);
+      }
+    }
+  },
+  (error) => {
+    Notify({ type: "danger", message: "网络错误，请刷新重试" });
+    return Promise.reject(error);
+  }
+);
